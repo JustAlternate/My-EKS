0546ede173dc498e16ac6396101a515cbf32b801
JustAlternate <loicw@justalternate.com>
2025-10-12 20:46:23 +0200

    fix: ecr force delete true

--- iac/ecr.tf
+++ iac/ecr.tf
@@ -3,4 +3,5 @@ resource "aws_ecr_repository" "repositories" {
   name     = each.key
   # image_tag_mutability = "IMMUTABLE"
   image_tag_mutability = "MUTABLE" //TODO: fix this to IMMUTABLE once you have proper release tags image build and pushing to ECR (ie = a CI/CD)
+  force_delete = true
 }
