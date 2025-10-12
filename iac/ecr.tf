d3192e71888dd33226775954742f6c3253a0dda3
JustAlternate <loicw@justalternate.com>
2025-10-12 19:37:55 +0200

    chores: use ECR MUTABLE rather than IMMUTABLE until I set up a proper release pipeline

--- iac/ecr.tf
+++ iac/ecr.tf
@@ -1,5 +1,6 @@
 resource "aws_ecr_repository" "repositories" {
   for_each = toset( ["api", "web-server"] )
   name     = each.key
-  image_tag_mutability = "IMMUTABLE"
+  # image_tag_mutability = "IMMUTABLE"
+  image_tag_mutability = "MUTABLE" //TODO: fix this to IMMUTABLE once you have proper release tags image build and pushing to ECR (ie = a CI/CD)
 }
