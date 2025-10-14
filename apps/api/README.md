0fb6d3bfd1276cea9a62d41398b1bd208c77997f
JustAlternate <64951265+JustAlternate@users.noreply.github.com>
2025-10-14 12:56:17 +0200

    feat: build push workflows (#2)
    * feat: build push workflows
    
    * wip

--- apps/api/README.md
+++ apps/api/README.md
@@ -1,5 +1,6 @@
 # Sample API
 - On `GET /`, log the request (will update a counter in a PG in the future)
 
+
 `docker build . -t api -f Containerfile`
 `docker run -p 3030:3030 api:latest`
--- iac/ecr.tf
+++ iac/ecr.tf
@@ -1,7 +1,58 @@
-resource "aws_ecr_repository" "repositories" {
+resource "aws_ecr_repository" "prod_repositories" {
   for_each = toset(["api", "web-server"])
-  name     = each.key
-  # image_tag_mutability = "IMMUTABLE"
-  image_tag_mutability = "MUTABLE" //TODO: fix this to IMMUTABLE once you have proper release tags image build and pushing to ECR (ie = a CI/CD)
+  name     = "${each.key}-prod"
+  image_tag_mutability = "IMMUTABLE"
   force_delete         = true
+}
+
+resource "aws_ecr_lifecycle_policy" "prod_repositories_policy" {
+  for_each = aws_ecr_repository.prod_repositories
+
+  repository = aws_ecr_repository.prod_repositories[each.key].name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Keep production images"
+        selection = {
+          tagStatus   = "any"
+          countType   = "imageCountMoreThan"
+          countNumber = 50
+        }
+        action = { type = "expire" }
+      }
+    ]
+  })
+}
+
+resource "aws_ecr_repository" "dev_repositories" {
+  for_each = toset(["api", "web-server"])
+  name     = "${each.key}-dev"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true
+}
+
+resource "aws_ecr_lifecycle_policy" "dev_repositories_policy" {
+  for_each = aws_ecr_repository.dev_repositories
+
+  repository = aws_ecr_repository.dev_repositories[each.key].name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Expire untagged images older than 1 day"
+        selection = {
+          tagStatus   = "untagged"
+          countType   = "sinceImagePushed"
+          countUnit   = "days"
+          countNumber = 1
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
 }
