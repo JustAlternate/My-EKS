b1d5318b0c5dda479d89b7fcf67c8dfa4c232d82
JustAlternate <loicw@justalternate.com>
2025-10-28 22:28:21 +0100

    feat: create a monitoring node group with special taint

--- iac/main.tf
+++ iac/main.tf
@@ -29,50 +29,6 @@ resource "aws_eks_cluster" "main" {
   }
 }
 
-# EKS Node Group
-resource "aws_eks_node_group" "main" {
-  cluster_name    = aws_eks_cluster.main.name
-  node_group_name = "${var.cluster_name}-nodes"
-  node_role_arn   = aws_iam_role.eks_nodes.arn
-  subnet_ids      = module.vpc.private_subnets
-
-  scaling_config {
-    desired_size = var.node_desired_size
-    max_size     = var.node_max_size
-    min_size     = var.node_min_size
-  }
-
-  update_config {
-    max_unavailable = 1
-  }
-
-  instance_types = var.node_instance_types
-  ami_type       = "AL2023_ARM_64_STANDARD" # ARM AMI for t4g instances
-  # release_version will be automatically set to match the cluster version
-
-  depends_on = [
-    aws_iam_role_policy_attachment.eks_nodes_AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.eks_nodes_AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.eks_nodes_AmazonEC2ContainerRegistryReadOnly,
-  ]
-
-  tags = {
-    Name = "${var.cluster_name}-node-group"
-  }
-
-  # Increase timeout for node group creation (can take 10-20 minutes)
-  timeouts {
-    create = "40m"
-    update = "40m"
-    delete = "40m"
-  }
-
-  # Force node group update when cluster version changes
-  lifecycle {
-    ignore_changes = [release_version]
-  }
-}
-
 # CloudWatch Log Group for EKS
 resource "aws_cloudwatch_log_group" "eks_cluster" {
   name              = "/aws/eks/${var.cluster_name}/cluster"
@@ -81,4 +37,12 @@ resource "aws_cloudwatch_log_group" "eks_cluster" {
   tags = {
     Name = "${var.cluster_name}-eks-logs"
   }
+}
+resource "aws_eks_addon" "ebs-csi" {
+  cluster_name                = aws_eks_cluster.main.name
+  addon_name                  = "aws-ebs-csi-driver"
+  addon_version               = "v1.51.0-eksbuild.1"
+  service_account_role_arn    = aws_iam_role.aws_ebs_csi_driver.arn
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
 }
