fccd79a941432c192147c4ba1a436251c757efad
JustAlternate <loicw@justalternate.com>
2025-11-02 11:59:08 +0100

    still issue with destroying vpc

---
+++ destroy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -e
+
+aws eks update-kubeconfig --region "${AWS_DEFAULT_REGION}" --name justalternate-eks-cluster 
+
+# Uninstall Helm releases
+echo "Removing all Helm releases..."
+helm uninstall promtail -n monitoring || true
+helm uninstall loki -n monitoring || true
+helm uninstall kube-prometheus-stack -n monitoring || true
+
+sleep 10
+
+kubectl delete pvc --all -n monitoring || true
+
+echo "Tofu destroy..."
+tofu -chdir ./iac destroy -auto-approve
--- iac/monitoring.tf
+++ iac/monitoring.tf
@@ -1,10 +1,8 @@
-# Data source for AWS account ID
-data "aws_caller_identity" "current" {}
-
 # Provider for Kubernetes resources
 provider "kubernetes" {
   host                   = aws_eks_cluster.main.endpoint
   cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority.0.data)
+  
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.main.name]
@@ -12,30 +10,11 @@ provider "kubernetes" {
   }
 }
 
-# Provider for Helm
-provider "helm" {
-  kubernetes = {
-    host                   = aws_eks_cluster.main.endpoint
-    cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority.0.data)
-    exec = {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.main.name]
-      command     = "aws"
-    }
-  }
-  # Add timeout settings
-  repository_config_path = "${path.module}/.helm/repositories.yaml"
-  repository_cache       = "${path.module}/.helm"
-  
-  # Increase timeout
-  burst_limit = 100
-}
-
 # EBS CSI Driver addon for EKS
 resource "aws_eks_addon" "ebs-csi" {
   cluster_name                = aws_eks_cluster.main.name
   addon_name                  = "aws-ebs-csi-driver"
-  addon_version               = "v1.37.0-eksbuild.1"  # Updated to latest stable
+  addon_version               = "v1.37.0-eksbuild.1"
   service_account_role_arn    = aws_iam_role.aws_ebs_csi_driver.arn
   resolve_conflicts_on_create = "OVERWRITE"
   resolve_conflicts_on_update = "OVERWRITE"
@@ -46,10 +25,10 @@ resource "aws_eks_addon" "ebs-csi" {
   ]
 }
 
+# Create monitoring namespace
 resource "kubernetes_namespace" "monitoring" {
   metadata {
     name = "monitoring"
-    
     labels = {
       name = "monitoring"
     }
@@ -59,105 +38,26 @@ resource "kubernetes_namespace" "monitoring" {
     aws_eks_cluster.main,
     aws_eks_node_group.monitoring
   ]
-
-  timeouts {
-    delete = "5m"
-  }
 }
 
-resource "helm_release" "kube_prometheus_stack" {
-  name             = "kube-prometheus-stack"
-  repository       = "https://prometheus-community.github.io/helm-charts"
-  chart            = "kube-prometheus-stack"
-  version          = "65.0.0"
-  namespace        = kubernetes_namespace.monitoring.metadata[0].name
-  create_namespace = false
-
-  values = [
-    file("${path.module}/observability-stack-config/prometheus-stack-values.yaml")
-  ]
-
-  # Set Grafana IAM role dynamically
-  set = [
-    {
-      name  = "grafana.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-      value = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/grafana-cloudwatch-role"
+# Create gp3 StorageClass
+resource "kubernetes_storage_class_v1" "gp3" {
+  metadata {
+    name = "gp3"
+    annotations = {
+      "storageclass.kubernetes.io/is-default-class" = "true"
     }
-  ]
-
-  atomic          = true
-  cleanup_on_fail = true
-  wait            = true
-  timeout         = 600
-
-  depends_on = [
-    aws_eks_addon.ebs-csi,
-    aws_eks_node_group.monitoring,
-    kubernetes_namespace.monitoring,
-    kubernetes_storage_class_v1.gp3
-  ]
-
-  lifecycle {
-    prevent_destroy = false
-  }
-}
-
-resource "helm_release" "loki" {
-  name             = "loki"
-  repository       = "https://grafana.github.io/helm-charts"
-  chart            = "loki"
-  version          = "6.6.0"
-  namespace        = kubernetes_namespace.monitoring.metadata[0].name
-  create_namespace = false
-
-  values = [
-    file("${path.module}/observability-stack-config/loki-values.yaml")
-  ]
-
-  atomic          = true
-  cleanup_on_fail = true
-  wait            = true  # Changed to true
-  timeout         = 600   # 10 minutes
-
-  timeouts = {
-    create = "10m"
-    update = "10m"
-    delete = "10m"
   }
 
-  depends_on = [
-    aws_eks_addon.ebs-csi,
-    aws_eks_node_group.monitoring,
-    kubernetes_namespace.monitoring,
-    helm_release.kube_prometheus_stack
-  ]
-}
+  storage_provisioner    = "ebs.csi.aws.com"
+  allow_volume_expansion = true
+  reclaim_policy         = "Delete"
+  volume_binding_mode    = "WaitForFirstConsumer"
 
-resource "helm_release" "promtail" {
-  name             = "promtail"
-  repository       = "https://grafana.github.io/helm-charts"
-  chart            = "promtail"
-  version          = "6.16.0"
-  namespace        = kubernetes_namespace.monitoring.metadata[0].name
-  create_namespace = false
-
-  values = [
-    file("${path.module}/observability-stack-config/promtail-values.yaml")
-  ]
-
-  atomic          = true
-  cleanup_on_fail = true
-  wait            = true
-  timeout         = 300
-
-  timeouts = {
-    create = "10m"
-    update = "10m"
-    delete = "10m"
+  parameters = {
+    type      = "gp3"
+    encrypted = "true"
   }
 
-  depends_on = [
-    helm_release.loki,  # Install after Loki
-    kubernetes_namespace.monitoring
-  ]
+  depends_on = [aws_eks_addon.ebs-csi]
 }
