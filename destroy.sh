4bcfd7a660a2087ed8c6ab925343a66e94bc56dc
JustAlternate <loicw@justalternate.com>
2025-11-19 14:59:09 +0100

    fix: chicken and egg problem with eks provisionning alb and stuff

--- destroy.sh
+++ destroy.sh
@@ -13,5 +13,9 @@ sleep 10
 
 kubectl delete pvc --all -n monitoring || true
 
+kubectl get svc --all-namespaces -o json | jq -r '.items[] | select(.spec.type=="LoadBalancer") | .metadata.name + " -n " + .metadata.namespace' | xargs -L 1 kubectl delete svc
+
+kubectl delete ingress --all --all-namespaces
+
 echo "Tofu destroy..."
 tofu -chdir=./iac destroy -auto-approve
