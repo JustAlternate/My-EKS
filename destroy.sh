9dd4d4296b44cfa27cd764829cc23d1abad09cd9
JustAlternate <loicw@justalternate.com>
2025-11-21 14:32:51 +0100

    fix

--- destroy.sh
+++ destroy.sh
@@ -13,7 +13,7 @@ sleep 10
 
 kubectl delete pvc --all -n monitoring || true
 
-kubectl get svc --all-namespaces -o json | jq -r '.items[] | select(.spec.type=="LoadBalancer") | .metadata.name + " -n " + .metadata.namespace' | xargs -L 1 kubectl delete svc
+kubectl delete svc web-server-service-loadbalancer
 
 kubectl delete ingress --all --all-namespaces
 
